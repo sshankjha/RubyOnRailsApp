d9c22620ba1035564c343b91f286f91418eb274d
sshankjha <shashankjha.np@gmail.com>
2017-03-01 00:58:56 -0500

    Close #35. Destroy account refreshes page rather than redirecting

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,7 +7,7 @@ class AccountsController < ApplicationController
     if current_user and Account.find_by_user_id(current_user[:id])
       @accounts = Account.where(current_user[:id])
     else
-      flash[:notice] = 'No accounts to view. Request one'
+      flash[:notice] = 'No accounts to view. Request one.'
       redirect_to user_path, {:id => current_user[:id]}
     end
   end
@@ -18,7 +18,7 @@ class AccountsController < ApplicationController
     if current_user and Account.find_by_user_id(current_user[:id])
       @accounts = Account.where(user_id: current_user[:id])
     else
-      flash[:notice] = 'No accounts to view. Request one'
+      flash[:notice] = 'No accounts to view. Request one.'
       redirect_to user_path
     end
   end
@@ -34,7 +34,7 @@ class AccountsController < ApplicationController
     @account = Account.new(acc_no: 100_000_000 + Random.rand(99_999_999) ,balance: 0.0, state: 0, user_id: current_user[:id])
     respond_to do |format|
       if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully requested' }
+        format.html { redirect_to @account, notice: 'Account was successfully requested.' }
         format.json { render :show, status: :created, location: @account }
       else
         format.html { render :new }
@@ -62,10 +62,9 @@ class AccountsController < ApplicationController
   # DELETE /accounts/1.json
   def destroy
     @account = Account.find(params[:id])
-    @account.destroy
-    respond_to do |format|
-      format.html { redirect_to current_user, notice: 'Account was successfully destroyed.' }
-      format.json { head :no_content }
+    if @account.destroy
+      flash[:notice] = 'Account was successfully destroyed.'
+      redirect_to :back
     end
   end
 
