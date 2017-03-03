043f836d74a8596957107026a52b42478bc50a5a
ruthvik1994 <mruthvik1994@gmail.com>
2017-03-02 22:39:29 -0500

    Issue#44 solved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -107,8 +107,12 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        if is_admin?
+          format.html { redirect_to admin_path(current_user[:id]), notice: 'User was successfully updated.' }
+        else
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        end
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
